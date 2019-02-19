521fc175afaabc507ab7ff52b5f111a061873fd0
Plamen Boudinov <boudinov@gmail.com>
2019-02-19 15:45:57 +0200

    clarify example

--- RoutingLocalization/WorkingExample/Controllers/InvestorController.cs
+++ RoutingLocalization/WorkingExample/Controllers/InvestorController.cs
@@ -9,18 +9,17 @@ namespace WorkingExample.Controllers
 {
     public class InvestorController : Controller
     {
-        //redirect /investing to /invest
-        [LocalizedRoute("~/investing", explicitCulture: "en", translateUrl: false)]
-        public ActionResult Index_OLD_EN()
-        {
-            return RedirectToActionPermanent("Index");
-        }
-
-
         [LocalizedRoute("~/invest")]
         public ActionResult Index()
         {
             return View();
+        }
+    
+        //example how to handle old urls in respect to SEO - redirect old /investing to new /invest url
+        [LocalizedRoute("~/investing", explicitCulture: "en", translateUrl: false)]
+        public ActionResult Index_OLD_EN()
+        {
+            return RedirectToActionPermanent("Index");
         }
     }
 }
